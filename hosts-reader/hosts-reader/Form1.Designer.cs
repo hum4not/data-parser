bdf2a3e1750a495775259a8c46c346c2317d71eb
hum4not <63611146+hum4not@users.noreply.github.com>
2022-03-09 11:41:25 +0500

    Add files via upload

---
+++ hosts-reader/hosts-reader/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace hosts_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.url = new System.Windows.Forms.TextBox();
+            this.logs = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.isFilter = new System.Windows.Forms.CheckBox();
+            this.isIp = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.BackColor = System.Drawing.SystemColors.MenuText;
+            this.url.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.url.ForeColor = System.Drawing.Color.Lime;
+            this.url.Location = new System.Drawing.Point(414, 12);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(374, 20);
+            this.url.TabIndex = 0;
+            // 
+            // logs
+            // 
+            this.logs.BackColor = System.Drawing.SystemColors.MenuText;
+            this.logs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logs.ForeColor = System.Drawing.Color.Lime;
+            this.logs.Location = new System.Drawing.Point(12, 12);
+            this.logs.Multiline = true;
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(375, 426);
+            this.logs.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(414, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // isFilter
+            // 
+            this.isFilter.AutoSize = true;
+            this.isFilter.ForeColor = System.Drawing.Color.Lime;
+            this.isFilter.Location = new System.Drawing.Point(414, 39);
+            this.isFilter.Name = "isFilter";
+            this.isFilter.Size = new System.Drawing.Size(48, 17);
+            this.isFilter.TabIndex = 3;
+            this.isFilter.Text = "Filter";
+            this.isFilter.UseVisualStyleBackColor = true;
+            // 
+            // isIp
+            // 
+            this.isIp.AutoSize = true;
+            this.isIp.ForeColor = System.Drawing.Color.Lime;
+            this.isIp.Location = new System.Drawing.Point(468, 38);
+            this.isIp.Name = "isIp";
+            this.isIp.Size = new System.Drawing.Size(36, 17);
+            this.isIp.TabIndex = 4;
+            this.isIp.Text = "IP";
+            this.isIp.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(637, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Made by humanot";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(275, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isIp);
+            this.Controls.Add(this.isFilter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.url);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Name = "Form1";
+            this.Text = "RetardParser v1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.TextBox logs;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox isFilter;
+        private System.Windows.Forms.CheckBox isIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hosts-reader/hosts-reader/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace hosts_reader
+{
+    public partial class Form1 : Form
+    {
+        public static int ghp { get; set; }
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            if (ghp == 240)
+            {
+                ghp = 0;
+            }
+            else
+            {
+                ghp++;
+
+
+                label1.ForeColor = ColorTranslator.FromWin32(ColorHLSToRGB(ghp, 100, 150));
+            }
+        }
+        [DllImport("shlwapi.dll")]
+        public static extern int ColorHLSToRGB(int H, int L, int S);
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void parse(string url)
+        {
+            if (isFilter.Checked == false) {
+                if (isIp.Checked == false)
+                {
+                    WebClient web = new WebClient();
+                    System.IO.Stream stream = web.OpenRead(url);
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        string text = reader.ReadToEnd();
+                        sendLog(text);
+                    }
+                }
+                else
+                {
+                    WebClient web = new WebClient();
+                    System.IO.Stream stream = web.OpenRead($"https://{url}/growtopia/server_data.php");
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        string text = reader.ReadToEnd();
+                        sendLog(text);
+                    }
+                }
+            }
+            else
+            {
+                if (isIp.Checked == false)
+                {
+                    WebClient web = new WebClient();
+                    System.IO.Stream stream = web.OpenRead(url);
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        string text = reader.ReadToEnd();
+                        string[] retard = text.Split(new string[] { "type|" }, StringSplitOptions.None);
+                        sendLog(retard[0]);
+                    }
+                }
+                else
+                {
+                    WebClient web = new WebClient();
+                    System.IO.Stream stream = web.OpenRead($"https://{url}/growtopia/server_data.php");
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        string text = reader.ReadToEnd();
+                        string[] retard = text.Split(new string[] { "type|" }, StringSplitOptions.None);
+                        sendLog(retard[0]);
+                    }
+                }
+            }
+
+        }
+        public void sendLog(string text)
+        {
+            logs.AppendText(text.Replace("\n",Environment.NewLine));
+            logs.AppendText(Environment.NewLine);
+            logs.AppendText(Environment.NewLine);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendLog($"Proccessing [{url.Text}]");
+            parse(url.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.youtube.com/channel/UCtF77sHHJwQt3B38X6m7rTw");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            logs.Clear();
+        }
+    }
+}
